14f4c1104c8f22a10058e35d0861ffe73d149351
Alonso del Arte <alonso.delarte@gmail.com>
2021-11-21 01:37:06 -0500

    Some groundwork for GUI testing

---
+++ test/ui/graphical/MockGraphicsTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2021 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package ui.graphical;
+
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Tests of the MockGraphics class.
+ * @author Alonso del Arte
+ */
+public class MockGraphicsTest {
+    
+}
