c4fbe740d5e4820723d7c13a53e604d1114a965c
Alonso del Arte <alonso.delarte@gmail.com>
2023-03-16 22:57:11 -0400

    Starting work on CardQueue

---
+++ test/playingcards/CardQueue.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2023 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package playingcards;
+
+/**
+ * Provides cards of specific ranks or suits. To be used for testing purposes 
+ * only.
+ * @author Alonso del Arte
+ */
+public class CardQueue implements CardSupplier {
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+    
+    public void cueUp(Rank rank) {
+        // TODO: Write tests for this
+    }
+
+    public void cueUp(Suit suit) {
+        // TODO: Write tests for this
+    }
+
+    @Override
+    public PlayingCard getNextCard() {
+        return new PlayingCard(Rank.JACK, Suit.CLUBS);
+    }
+
+    @Override
+    public boolean provenance(PlayingCard card) {
+        return false;
+    }
+    
+}
