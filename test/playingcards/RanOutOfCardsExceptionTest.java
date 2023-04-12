f6d6f8a847568910ed5c62775504545c6bd9ffbd
Alonso del Arte <alonso.delarte@gmail.com>
2023-04-12 13:42:26 -0400

    Failing zero-parameter constructor test

--- test/playingcards/RanOutOfCardsExceptionTest.java
+++ test/playingcards/RanOutOfCardsExceptionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Alonso del Arte
+ * Copyright (C) 2023 Alonso del Arte
  *
  * This program is free software: you can redistribute it and/or modify it under 
  * the terms of the GNU General Public License as published by the Free Software 
@@ -26,94 +26,115 @@ import static org.junit.Assert.*;
 public class RanOutOfCardsExceptionTest {
     
     /**
-     * Test of rankDeficient method, of class RanOutOfCardsException.
+     * Test of the rankDeficient function, of the RanOutOfCardsException class.
      */
     @Test
     public void testRankDeficient() {
         System.out.println("rankDeficient");
-        RanOutOfCardsException roce = new RanOutOfCardsException("Ran out of Jacks", Rank.JACK);
-        assert roce.rankDeficient() 
-                : "Exception for ran out of Jacks should be considered rank-deficient";
+        RanOutOfCardsException roce 
+                = new RanOutOfCardsException("Ran out of Jacks", Rank.JACK);
+        String msg = "Ran out of Jacks should be considered rank-deficient";
+        assert roce.rankDeficient() : msg;
     }
 
     /**
-     * Another test of rankDeficient method, of class RanOutOfCardsException.
+     * Another test of the rankDeficient function, of the RanOutOfCardsException 
+     * class.
      */
     @Test
     public void testNotRankDeficient() {
-        RanOutOfCardsException roce = new RanOutOfCardsException("Ran out of all cards");
-        assert !roce.rankDeficient() 
-                : "Exception for ran out of all cards should not be considered rank-deficient";
+        RanOutOfCardsException roce 
+                = new RanOutOfCardsException("Ran out of all cards");
+        String msg = "Ran out of all cards not considered rank-deficient";
+        assert !roce.rankDeficient() : msg;
     }
 
     /**
-     * Test of suitDeficient method, of class RanOutOfCardsException.
+     * Test of the suitDeficient function, of the RanOutOfCardsException class.
      */
     @Test
     public void testSuitDeficient() {
         System.out.println("suitDeficient");
-        RanOutOfCardsException roce = new RanOutOfCardsException("Ran out of Clubs", Suit.CLUBS);
-        assert roce.suitDeficient() 
-                : "Exception for ran out of Clubs should be considered suit-deficient";
+        RanOutOfCardsException roce 
+                = new RanOutOfCardsException("Ran out of Clubs", Suit.CLUBS);
+        String msg = "Ran out of Clubs should be considered suit-deficient";
+        assert roce.suitDeficient() : msg;
     }
 
     /**
-     * Another test of suitDeficient method, of class RanOutOfCardsException.
+     * Another test of the suitDeficient function, of the RanOutOfCardsException 
+     * class.
      */
     @Test
     public void testNotSuitDeficient() {
-        RanOutOfCardsException roce = new RanOutOfCardsException("Ran out of all cards");
-        assert !roce.suitDeficient()
-                : "Exception for ran out of all cards should not be considered suit-deficient";
+        RanOutOfCardsException roce 
+                = new RanOutOfCardsException("Ran out of all cards");
+        String msg = "Ran out of all cards not considered suit-deficient";
+        assert !roce.suitDeficient() : msg;
     }
 
     /**
-     * Test of getRank method, of class RanOutOfCardsException.
+     * Test of the getRank function, of the RanOutOfCardsException class.
      */
     @Test
     public void testGetRank() {
         System.out.println("getRank");
         Rank expected = Rank.NINE;
-        RanOutOfCardsException roce = new RanOutOfCardsException("Ran out of Nines", expected);
+        RanOutOfCardsException roce 
+                = new RanOutOfCardsException("Ran out of Nines", expected);
         Rank actual = roce.getRank();
         assertEquals(expected, actual);
     }
 
     /**
-     * Another test of getRank method, of class RanOutOfCardsException. If the 
-     * exception was constructed without specifying a rank deficiency, the 
-     * getter should return a null pointer.
+     * Another test of the getRank function, of the RanOutOfCardsException 
+     * class. If the exception was constructed without specifying a rank 
+     * deficiency, the getter should return a null pointer.
      */
     @Test
     public void testGetRankShouldNotInventRank() {
-        RanOutOfCardsException roce = new RanOutOfCardsException("Ran out of Hearts", Suit.HEARTS);
+        RanOutOfCardsException roce 
+                = new RanOutOfCardsException("Ran out of Hearts", Suit.HEARTS);
         Rank rank = roce.getRank();
         assertNull(rank);
     }
 
     /**
-     * Test of getSuit method, of class RanOutOfCardsException.
+     * Test of the getSuit function, of the RanOutOfCardsException class.
      */
     @Test
     public void testGetSuit() {
         System.out.println("getSuit");
         Suit expected = Suit.HEARTS;
-        RanOutOfCardsException roce = new RanOutOfCardsException("Ran out of Hearts", expected);
+        RanOutOfCardsException roce 
+                = new RanOutOfCardsException("Ran out of Hearts", expected);
         Suit actual = roce.getSuit();
         assertEquals(expected, actual);
     }
     
     /**
-     * Another test of getSuit method, of class RanOutOfCardsException. If the 
-     * exception was constructed without specifying a suit deficiency, the 
-     * getter should return a null pointer.
+     * Another test of the getSuit function, of the RanOutOfCardsException 
+     * class. If the exception was constructed without specifying a suit 
+     * deficiency, the getter should return a null pointer.
      */
     @Test
     public void testGetSuitShouldNotInventSuit() {
         System.out.println("getSuit");
-        RanOutOfCardsException roce = new RanOutOfCardsException("Ran out of Hearts", Rank.NINE);
+        RanOutOfCardsException roce 
+                = new RanOutOfCardsException("Ran out of Hearts", Rank.NINE);
         Suit suit = roce.getSuit();
         assertNull(suit);
+    }
+    
+    @Test
+    public void testZeroParamConstructor() {
+        RanOutOfCardsException exc = new RanOutOfCardsException();
+        String expected = "No cards of any rank or suit left";
+        String actual = exc.getMessage();
+        assertEquals(expected, actual);
+        String msg = "With no cards whatsoever, no rank, suit should be listed";
+        assertNull(msg, exc.getRank());
+        assertNull(msg, exc.getSuit());
     }
     
 }
