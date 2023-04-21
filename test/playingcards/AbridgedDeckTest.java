d45a6553e2c55fc21073eb77e62c7a43252db89f
Alonso del Arte <alonso.delarte@gmail.com>
2023-04-20 21:05:56 -0400

    New class

---
+++ test/playingcards/AbridgedDeckTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2023 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package playingcards;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Tests of the AbridgedDeck class.
+ * @author Alonso del Arte
+ */
+public class AbridgedDeckTest {
+    
+    @Test
+    public void testOmitSingleRank() {
+        for (Rank omittedRank : Rank.values()) {
+            AbridgedDeck deck = new AbridgedDeck(omittedRank);
+            deck.shuffle();
+            String msgPart = " should not be " + omittedRank.getWord();
+            while (deck.hasNext()) {
+                PlayingCard card = deck.getNextCard();
+                String msg = card.toString() + msgPart;
+                assert card.getRank() != omittedRank : msg;
+            }
+        }
+    }
+    
+//    @Test
+    public void testOmitSingleSuit() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
