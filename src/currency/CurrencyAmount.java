620ac95a8a5e256d513d5c156998053e5743fb1a
Alonso del Arte <alonso.delarte@gmail.com>
2023-07-18 16:13:14 -0400

    Passing is not positive but it is test

--- src/currency/CurrencyAmount.java
+++ src/currency/CurrencyAmount.java
@@ -133,9 +133,13 @@ public class CurrencyAmount implements Comparable<CurrencyAmount>,
         return Long.signum(diff.amountInCents);
     }
     
-    // TODO: Write tests for this
+    /**
+     * Determines whether this currency amount is not positive.
+     * @return True if the amount is negative, false otherwise. For example, 
+     * true for &minus;$3.44, false for 89,20&euro;.
+     */
     public boolean isNotPositive() {
-        return true;
+        return this.amountInCents < 0;
     }
     
     public static CurrencyAmount parseAmount(String s) {
