e8627f01faa471f67cd9092233a38d35ec70fc89
Alonso del Arte <alonso.delarte@gmail.com>
2023-07-18 16:07:23 -0400

    Passing isNotPositive( ) test

--- src/currency/CurrencyAmount.java
+++ src/currency/CurrencyAmount.java
@@ -13,7 +13,7 @@
  *
  * You should have received a copy of the GNU General Public License along with 
  * this program; if not, write to the Free Software Foundation, Inc., 59 Temple 
- * Place - Suite 330, Boston, MA  02111-1307, USA.
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 package currency;
 
@@ -135,7 +135,7 @@ public class CurrencyAmount implements Comparable<CurrencyAmount>,
     
     // TODO: Write tests for this
     public boolean isNotPositive() {
-        return false;
+        return true;
     }
     
     public static CurrencyAmount parseAmount(String s) {
