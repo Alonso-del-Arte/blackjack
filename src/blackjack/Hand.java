d1fc742c9917a2ee9b15284a5a3b5e30fa422e70
Alonso del Arte <alonso.delarte@gmail.com>
2023-08-18 20:24:08 -0400

    Stub

--- src/blackjack/Hand.java
+++ src/blackjack/Hand.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Alonso del Arte
+ * Copyright (C) 2023 Alonso del Arte
  *
  * This program is free software: you can redistribute it and/or modify it under 
  * the terms of the GNU General Public License as published by the Free Software 
@@ -82,6 +82,11 @@ public class Hand {
      */
     public int cardsValue() {
         return this.handScore;
+    }
+    
+    // TODO: Write tests for this
+    public int cardCount() {
+        return Integer.MAX_VALUE;
     }
     
     /**
