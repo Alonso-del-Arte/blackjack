ea3871960cfa7a9c8ffd7666aad03fecc71f63bb
Alonso del Arte <alonso.delarte@gmail.com>
2025-02-17 00:42:46 -0500

    Back to passing constructor rejects empty name test

--- src/blackjack/Player.java
+++ src/blackjack/Player.java
@@ -115,10 +115,10 @@ public class Player {
 //            String excMsg = "Player name must not be null";
 //            throw new NullPointerException(excMsg);
 //        }
-//        if (name.equals("")) {
-//            String excMsg = "Player name must not be empty";
-//            throw new IllegalArgumentException(excMsg);
-//        }
+        if (name.equals("")) {
+            String excMsg = "Player name must not be empty";
+            throw new IllegalArgumentException(excMsg);
+        }
 //        if (initialBankroll.isNotPositive()) {
 //            String excMsg = "Initial bankroll should be positive, not " 
 //                    + initialBankroll.toString();
