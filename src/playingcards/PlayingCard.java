8490e6370d03f7673708ea864cfaebdc4239ff85
Alonso del Arte <alonso.delarte@gmail.com>
2025-03-11 20:46:03 -0400

    Rewind equals( ) to fail last test

--- src/playingcards/PlayingCard.java
+++ src/playingcards/PlayingCard.java
@@ -35,7 +35,7 @@ public class PlayingCard {
 
     @Override
     public boolean equals(Object obj) {
-        return obj instanceof PlayingCard;
+        return obj != null;// instanceof PlayingCard;
     }
 
     // TODO: Rewrite tests for this
