baa827492bf41059889178db5cf662abd2874331
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-16 21:48:56 -0500

    Random instance for this package

--- test/playingcards/PlayingCardTest.java
+++ test/playingcards/PlayingCardTest.java
@@ -19,6 +19,7 @@ package playingcards;
 import java.awt.Color;
 import java.util.HashSet;
 import java.util.Locale;
+import java.util.Random;
 import java.util.ResourceBundle;
 import java.util.Set;
 
@@ -30,6 +31,8 @@ import static org.junit.Assert.*;
  * @author Alonso del Arte
  */
 public class PlayingCardTest {
+    
+    static final Random RANDOM = new Random(System.currentTimeMillis() << 4);
     
     private static final char HIGH_SURROGATE = '\uD83C';
     
