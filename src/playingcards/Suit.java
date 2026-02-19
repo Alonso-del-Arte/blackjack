ea402bc597937d6b770fbe46abb6eb47b33e7fe3
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-19 00:20:35 -0500

    Temporarily rewind parseSuit( ) to failing

--- src/playingcards/Suit.java
+++ src/playingcards/Suit.java
@@ -198,10 +198,10 @@ public enum Suit implements CardSpec {
             case "Hearts"-> Suit.HEARTS;
             case "Clubs" -> Suit.CLUBS;
             case "Diamonds" -> Suit.DIAMONDS;
-            default -> {
-                String excMsg = "No matching suit found for \"" + s + "\"";
-                throw new NoSuchElementException(excMsg);
-            }
+            default -> Suit.SPADES;// {
+//                String excMsg = "No matching suit found for \"" + s + "\"";
+//                throw new NoSuchElementException(excMsg);
+//            }
         };
     }
     
