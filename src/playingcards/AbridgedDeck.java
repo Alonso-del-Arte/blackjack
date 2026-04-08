1d8df5ad566a0414479a53100c8e58650a9c6c58
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-08 17:07:43 -0400

    Back to passing all tests so far

--- src/playingcards/AbridgedDeck.java
+++ src/playingcards/AbridgedDeck.java
@@ -31,7 +31,7 @@ public final class AbridgedDeck extends CardDeck {
     // TODO: Write tests for this
     @Override
     public boolean hasNext() {
-        return false;// this.dealCount < CardDeck.INITIAL_NUMBER_OF_CARDS_PER_DECK;
+        return this.dealCount < CardDeck.INITIAL_NUMBER_OF_CARDS_PER_DECK;
     }
     
     // TODO: Write tests for this
