affd1c08d8894a310c50b7a6e9ed2b7571fc0c92
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-04 21:44:38 -0400

    Passing shuffle( ) test for deck with suits omitted

--- src/playingcards/AbridgedDeck.java
+++ src/playingcards/AbridgedDeck.java
@@ -60,6 +60,9 @@ public final class AbridgedDeck extends CardDeck {
             if (this.ranksOmitted) {
                 Collections.shuffle(this.cards);
             }
+            if (this.suitsOmitted) {
+                Collections.shuffle(this.cards);
+            }
         } else {
             switch (this.dealCount) {
                 case 0 -> Collections.shuffle(this.cards);
