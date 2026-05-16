90271c642d5ddc539874fc646a500b40be9c074e
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-15 20:09:07 -0400

    Passing cardsValue( ) test but failing two others

--- src/blackjack/Hand.java
+++ src/blackjack/Hand.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 Alonso del Arte
+ * Copyright (C) 2026 Alonso del Arte
  *
  * This program is free software: you can redistribute it and/or modify it under 
  * the terms of the GNU General Public License as published by the Free Software 
@@ -79,7 +79,7 @@ public class Hand {
      * because a hand with an Ace as the only card is valued at 11.
      */
     public int cardsValue() {
-        return -this.handScore;
+        return this.handScore;
     }
     
     // TODO: Write tests for this
