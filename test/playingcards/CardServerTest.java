3b3791930ea01ea6230d40805088d8e321dfa8c4
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-19 01:54:27 -0400

    Restored previous higher requirement for one test

--- test/playingcards/CardServerTest.java
+++ test/playingcards/CardServerTest.java
@@ -290,7 +290,7 @@ public class CardServerTest {
         CardServer instance = new CardServer(DEFAULT_DECK_QUANTITY);
         PredicateWithDescription pwd = inventPredicate();
         Predicate<PlayingCard> predicate = pwd.predicate;
-        int numberOfCalls = 13;
+        int numberOfCalls = 26;
         int counter = 0;
         do {
             PlayingCard card = instance.giveCard(predicate);
