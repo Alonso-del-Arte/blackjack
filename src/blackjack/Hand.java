06aeed7657339365f116cd9086de40f35427d345
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-09 16:09:56 -0400

    Temporarily rewind isSplittableHand( ) to failing

--- src/blackjack/Hand.java
+++ src/blackjack/Hand.java
@@ -112,14 +112,14 @@ public class Hand {
      * (which some British casinos might enforce).
      */
     public boolean isSplittableHand(Dealer dealer) {
-        if (this.cards.size() == 2) {
-            RankPairSpec pairSpec 
-                    = new RankPairSpec(this.cards.get(0).getRank(), 
-                            this.cards.get(1).getRank());
-            return dealer.giveSplittablePairs().contains(pairSpec);
-        } else {
+//        if (this.cards.size() == 2) {
+//            RankPairSpec pairSpec 
+//                    = new RankPairSpec(this.cards.get(0).getRank(), 
+//                            this.cards.get(1).getRank());
+//            return dealer.giveSplittablePairs().contains(pairSpec);
+//        } else {
             return false;
-        }
+//        }
     }
     
     /**
