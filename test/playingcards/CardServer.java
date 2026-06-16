f6a4c01dea6e5bc0185b4de8cc6ed6611574e2cf
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-15 21:54:51 -0400

    More dramatic test failure

--- test/playingcards/CardServer.java
+++ test/playingcards/CardServer.java
@@ -180,7 +180,7 @@ public class CardServer implements CardSupplier {
      * server with more decks.
      */
     public PlayingCard giveCard(Predicate<PlayingCard> predicate) {
-        this.dealCount -= 2;
+        this.dealCount -= 120;
         while (this.hasNext()) {
             PlayingCard card = this.getNextCard();
             if (predicate.test(card)) {
