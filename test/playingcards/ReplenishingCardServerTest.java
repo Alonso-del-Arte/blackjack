3f4f43dd8fe7d493908987f2a21268b10f8153f7
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-21 21:48:52 -0400

    New class

---
+++ test/playingcards/ReplenishingCardServerTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2026 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package playingcards;
+
+import java.awt.Color;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import static org.testframe.api.Asserters.assertDoesNotThrow;
+import static org.testframe.api.Asserters.assertMaximum;
+import static org.testframe.api.Asserters.assertThrows;
+import static org.testframe.api.Asserters.assertZero;
+
+import static playingcards.PlayingCardTest.RANDOM;
+
+/**
+ * Tests of the ReplenishingCardServer class.
+ * @author Alonso del Arte
+ */
+public class ReplenishingCardServerTest {
+    
+}
