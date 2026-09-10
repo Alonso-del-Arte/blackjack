ef24616a3809cf3e1e6f95f12410a6aedc57c067
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-09 20:53:22 -0400

    Failing test choose currency no subdivisions

--- test/currency/CurrencyChooserTest.java
+++ test/currency/CurrencyChooserTest.java
@@ -217,6 +217,16 @@ public class CurrencyChooserTest {
     }
     
     @Test
+    public void testChooseCurrencyWithNoCentsOrDarahim() {
+        int expected = 0;
+        Currency currency = CurrencyChooser.chooseCurrency(expected);
+        int actual = currency.getDefaultFractionDigits();
+        String message = "Chosen currency " + currency.getDisplayName() 
+                + " should have " + expected + " default fraction digits";
+        assertEquals(message, expected, actual);
+    }
+
+    @Test
     public void testChooseCurrencyByBadPredicateCausesException() {
         String invalidDisplayName = "Invalid display name " 
                 + System.currentTimeMillis();
