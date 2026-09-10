d2c3190f4b154ac6914249aed11b070d9a4daf0d
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-09 20:55:26 -0400

    Failing test choose currency that divides into 100 cents

--- test/currency/CurrencyChooserTest.java
+++ test/currency/CurrencyChooserTest.java
@@ -227,6 +227,16 @@ public class CurrencyChooserTest {
     }
 
     @Test
+    public void testChooseCurrencyWith100Cents() {
+        int expected = 2;
+        Currency currency = CurrencyChooser.chooseCurrency(expected);
+        int actual = currency.getDefaultFractionDigits();
+        String message = "Chosen currency " + currency.getDisplayName() 
+                + " should have " + expected + " default fraction digits";
+        assertEquals(message, expected, actual);
+    }
+
+    @Test
     public void testChooseCurrencyByBadPredicateCausesException() {
         String invalidDisplayName = "Invalid display name " 
                 + System.currentTimeMillis();
