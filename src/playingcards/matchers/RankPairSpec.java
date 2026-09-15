a4024d044aa0f478fcc4d41cec6ff9b5c6e89427
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-15 01:14:35 -0400

    Back to passing not equals diff class test

--- src/playingcards/matchers/RankPairSpec.java
+++ src/playingcards/matchers/RankPairSpec.java
@@ -47,7 +47,7 @@ public class RankPairSpec extends PairSpec<Rank> {
     // TODO: Rewrite tests for this
     @Override
     public boolean equals(Object obj) {
-        return obj != null;
+        return obj instanceof RankPairSpec;
     }
     
     // TODO: Rewrite tests for this
